1716b509b7ab60a8b8ecd598b1f5ced0e4e63c83
MetalDestructor <rapid3fighter@hotmail.com>
2019-07-31 17:54:37 +0300

    Complete submission 3 - directives

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showDetails = false;
+  buttonLog = [];
+  limit = 4;
+
+  displayDetails() {
+    this.showDetails = !this.showDetails;
+    this.buttonLog.push(this.getCurrentTimestamp());
+  }
+
+  getCurrentTimestamp() {
+    let timestamp = new Date();
+
+    let dd = timestamp.getDate();
+    let mm = timestamp.getMonth() + 1;
+    let yyyy = timestamp.getFullYear();
+
+    let hh = timestamp.getHours();
+    let MM = timestamp.getMinutes();
+    let ss = timestamp.getSeconds();
+
+    let currentDate = mm + '/' + dd + '/' + yyyy;
+    let currentHour = hh + ':' + MM + ':' + ss;
+
+    return currentDate + ' ' + currentHour;
+  }
+
+  getBackgroundColor(i: number): string {
+    return i < this.limit ? 'white' : 'blue';
+  }
+
+  getClass(i: number): boolean {
+    return i < this.limit ? false : true;
+  }
 }
