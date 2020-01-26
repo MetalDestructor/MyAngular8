382762b99918f217efff839cbd507f7b321ac732
MetalDestructor <rapid3fighter@hotmail.com>
2020-01-26 06:13:45 +0200

    Store recipes in firebase db

--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -5,23 +5,29 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
+  recipes: Recipe[] = [];
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      "A Test Recipe",
-      "Test Description",
-      "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Froasted-red-pepper-deviled-eggs-ay.jpg%3Fitok%3DomK6dKy7&w=450&c=sc&poi=face&q=85",
-      [new Ingredient('asd', 12), new Ingredient('asd2', 20)]
-    ),
-    new Recipe(
-      "A Test Recipe2",
-      "Test Description",
-      "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Froasted-red-pepper-deviled-eggs-ay.jpg%3Fitok%3DomK6dKy7&w=450&c=sc&poi=face&q=85",
-      [new Ingredient('qwe', 12), new Ingredient('qwe2', 20)]
-    )
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     "A Test Recipe",
+  //     "Test Description",
+  //     "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Froasted-red-pepper-deviled-eggs-ay.jpg%3Fitok%3DomK6dKy7&w=450&c=sc&poi=face&q=85",
+  //     [new Ingredient('asd', 12), new Ingredient('asd2', 20)]
+  //   ),
+  //   new Recipe(
+  //     "A Test Recipe2",
+  //     "Test Description",
+  //     "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Froasted-red-pepper-deviled-eggs-ay.jpg%3Fitok%3DomK6dKy7&w=450&c=sc&poi=face&q=85",
+  //     [new Ingredient('qwe', 12), new Ingredient('qwe2', 20)]
+  //   )
+  // ];
 
   constructor(private shoppingListService: ShoppingListService){}
+
+  setRecipes(recipes: Recipe[]){
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
